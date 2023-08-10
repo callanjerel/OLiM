38379aeb1908b1ca322a75d6e6236b0bb3100909
callanjerel <333221.rob@gmail.com>
2023-08-10 13:19:20 -0600

    changes to use the correct server side stuff

--- server/server.js
+++ server/server.js
@@ -9,6 +9,7 @@ const port = 6060
 
 app.use(cors())
 app.use(express.json())
+app.use(express.static(path.resolve('../website/index.html')))
 
 const pageData = {}
 
--- website/scripts/index.js
+++ website/scripts/index.js
@@ -1,14 +1,14 @@
 const button = document.getElementById('getApi')
 const para = document.getElementById('text')
 
-button.addEventListener('click', (event) => {
-    fetch('http://localhost:6060/generateUniquePage')
-        .then(response => response.json())
-        .then(data => {
-            fetch("http://localhost:6060"+data.uniqueURL)
-                .then(resp => resp.text())
-                .then(content => {
-                    para.textContent = content
-                })
-        })
-})
+const socket = io.connect('http://localhost:6060');
+
+socket.on('connect', function() {
+    console.log('Connected to the server.');
+});
+
+// Listen for any custom events from the server
+socket.on('chat message', function(msg) {
+    // Handle the incoming message, maybe display it on the page
+    console.log('Received:', msg);
+});
