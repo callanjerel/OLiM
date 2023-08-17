44a90e5e9c18e9c31fce8b4a2ae926b883204b17
callanjerel <333221.rob@gmail.com>
2023-08-16 18:16:22 -0600

    Update socket.js

--- server/socket.js
+++ server/socket.js
@@ -1,29 +1,25 @@
 const dal = require('./datahandler/datalayer.js')
 
+const addMessage = (data) => { // Temporary solution to adding messages to queue
+    data.timestamp = '12:00:00'
+    data.user_id = '0'
+    data.content = data.body
+    dal.messages.create(false, data, (err, result) => {
+        if (err) {
+            console.error(err)
+            return
+        }
+        
+    })
+}
 
 
 module.exports = (io) => {
     const recieveMessage = (data) => {
         console.log("Message recieved: " + data.body)
-        data.timestamp = '12:00:00'
-        dal.chatRooms.create(false, {
-            id:"thisisthefirstroom",
-            admin_user_id:"0",
-            invite_code:"",
-            users:"",
-            messages:[
-                data
-            ]
-        }, (err, result) => {
-            if (err) {
-                console.error(err)
-                return
-            }
-            console.log(result)
-        })
         io.emit('chat message', data)
     }
-
+    // addMessage(data) <- This is commented :)
     io.on('connection', (socket) => {
         console.log('user connected')
     
